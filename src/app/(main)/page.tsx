27017e21ac2bc21168ff3c366a7cb30ebb7a7cf1
MamedovMurad <56360710+MamedovMurad@users.noreply.github.com>
2024-03-27 12:46:30 +0400

    added link"

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,73 +1,89 @@
-
-import Subscription from '@/components/general/subscription'
-
+import Subscription from "@/components/general/subscription";
 
 import { api } from "@/common/api";
 import { productDetail } from "@/common/model/products";
-import Link from 'next/link';
-
+import Link from "next/link";
 
 export default async function Home() {
-
-
-
   const data = await getData();
   if (!data?.data) {
-    return
+    return;
   }
   return (
     <>
-      {
-        data.data.map((item, index: number) => (
-          <section key={index} style={{ backgroundImage: `url(${item.image})` }} className='h-screen  relative  md:bg-[center_top_-9rem]  bg-no-repeat bg-cover flex items-center' >
+      {data.data.map((item, index: number) => (
+        <section
+          key={index}
+          style={{ backgroundImage: `url(${item.image})` }}
+          className="h-screen  relative  md:bg-[center_top_-9rem]  bg-no-repeat bg-cover flex items-center"
+        >
+          <div
+            className={
+              "container mx-auto flex  flex-col items-center    md:justify-normal md:h-min h-[80%]" +
+              (index === 0 ? " md:block  justify-end" : " justify-center")
+            }
+          >
+            <h1
+              className={
+                "  text-[48px] text-white leading-[3rem]  md:leading-[100px] md:text-[100px]" +
+                (index === 0
+                  ? " md:w-[550px] w-[230px]"
+                  : " w-full text-center md:w-full")
+              }
+            >
+              {item.title}{" "}
+            </h1>
+            {index !== 0 && (
+              <p className=" mt-2 text-white font-light  text-[27px] ">
+                {item.sub_title}
+              </p>
+            )}
+            {index === 0 && (
+              <Subscription
+                link={item.link_1}
+                className="w-[380px] md:mt-12  mt-64"
+                text={<p className="text-white">stay up to date with us</p>}
+              />
+            )}
+          </div>
 
-
-            <div className={'container mx-auto flex  flex-col items-center    md:justify-normal md:h-min h-[80%]' + (index === 0 ? " md:block  justify-end" : ' justify-center')}>
-              <h1 className={'  text-[48px] text-white leading-[3rem]  md:leading-[100px] md:text-[100px]' +
-                (index === 0 ? " md:w-[550px] w-[230px]" : ' w-full text-center md:w-full')}>{item.title} </h1>
-              {index !== 0 && <p className=' mt-2 text-white font-light  text-[27px] '>{item.sub_title}</p>}
-              {index === 0 && <Subscription className="w-[380px] md:mt-12  mt-64" text={<p className='text-white'>stay up to date with us</p>} />}
-
-            </div>
-
-
-            {index !== 0 && <div className=' absolute bottom-0 left-0 w-full  text-white backdrop-blur-lg' style={{ backgroundColor: 'rgba(0, 0, 0, 0.25)' }}>
-              <ul className='flex items-center justify-center md:py-[50px] py-5'>
-                <li className=' text-white md:text-[24px] text-[14px] font-light border-r-[1.5px] pr-8 border-white'>
+          {index !== 0 && (
+            <div
+              className=" absolute bottom-0 left-0 w-full  text-white backdrop-blur-lg"
+              style={{ backgroundColor: "rgba(0, 0, 0, 0.25)" }}
+            >
+              <ul className="flex items-center justify-center md:py-[50px] py-5">
+                <li className=" text-white md:text-[24px] text-[14px] font-light border-r-[1.5px] pr-8 border-white">
                   <Link href={item.link_1}>{item.link_title_1} </Link>
                 </li>
-                <li className=' text-white md:text-[24px] text-[14px] font-light  pl-8 '>
-                  <Link href={item.link_2}>{item.link_title_2}  </Link> </li>
+                <li className=" text-white md:text-[24px] text-[14px] font-light  pl-8 ">
+                  <Link href={item.link_2}>{item.link_title_2} </Link>{" "}
+                </li>
               </ul>
-            </div>}
-          </section>
-        ))
-      }
+            </div>
+          )}
+        </section>
+      ))}
     </>
-  )
-
-
+  );
 }
 
-
 async function getData() {
-
   const res = await api.get<{
     data: {
-      "title": string,
-      "sub_title": string,
-      "link_1": string,
-      "link_2": string,
-      "image": string,
-      link_title_1: string,
-      link_title_2: string
-    }[]
-  }>("", { cache: 'no-store' })
+      title: string;
+      sub_title: string;
+      link_1: string;
+      link_2: string;
+      image: string;
+      link_title_1: string;
+      link_title_2: string;
+    }[];
+  }>("", { cache: "no-store" });
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
   console.log(res?.data);
 
-
-  return res
+  return res;
 }
--- src/components/general/subscription/index.tsx
+++ src/components/general/subscription/index.tsx
@@ -1,19 +1,36 @@
-import { ArrowIcon, LetterIcon } from '@/svg';
-import React, { ReactNode } from 'react';
+import { ArrowIcon, LetterIcon } from "@/svg";
+import Router from "next/router";
+import React, { ReactNode } from "react";
 
 interface Props {
-    className?: string,
-    text: ReactNode
+  className?: string;
+  text: ReactNode;
+  link?: string;
 }
 
-const Subscription: React.FC<Props> = ({ className = "w-full", text }) => {
-    return (
-        <div className={className + ' border-[1px] border-white border-solid flex items-center justify-between p-[20px]'}>
-            <LetterIcon />
-            {text}
-            <ArrowIcon />
-        </div>
-    );
+const Subscription: React.FC<Props> = ({
+  className = "w-full",
+  text,
+  link,
+}) => {
+  function handleClick() {
+    if (link) {
+      window.location.href = link;
+    }
+  }
+  return (
+    <div
+      onClick={handleClick}
+      className={
+        className +
+        " border-[1px] border-white border-solid flex items-center justify-between p-[20px] cursor-pointer"
+      }
+    >
+      <LetterIcon />
+      {text}
+      <ArrowIcon />
+    </div>
+  );
 };
 
 export default Subscription;
